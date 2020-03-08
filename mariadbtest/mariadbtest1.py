5869050a6b04d9d53124b06c887b02ad702ef99e
shimseonjo <shimseonjo@gmail.com>
2020-03-08 10:40:25 +0900

    mariadb테스트

--- mariadbtest/mariadbtest1.py
+++ mariadbtest/mariadbtest1.py
@@ -1,12 +1,20 @@
 import pymysql
 
-conn = pymysql.connect(host='localhost',
+conn = pymysql.connect(host='maria',
                         user='root',
                         password='qwer1234',
                         db='test',
                         cursorclass=pymysql.cursors.DictCursor)
 
 c=conn.cursor()
-
-
+c.execute('''
+create table if not exists stocks
+(date text,trans text,symbol text,qty real,price real)
+''')
+c.execute('''
+insert into stocks 
+values('2020-01-05','buy','rhat',100,35.14)
+''')
+conn.commit()
+conn.close()
 
