a28487c4206f80d7ead1f41787855343a7b58691
shimseonjo <shimseonjo@gmail.com>
2020-03-14 15:11:49 +0900

    bootstrap 테스트

--- bootstraptest/app.py
+++ bootstraptest/app.py
@@ -6,5 +6,21 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('index copy.html')
+
+@app.route('/form')
+def inputform():
+    return render_template('form.html')
+
+@app.route('/inputdata')
+def inputdata():
+    return "inputdata"
+
+@app.route('/list')
+def listdata():
+    return render_template('list.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8890,debug=True)
